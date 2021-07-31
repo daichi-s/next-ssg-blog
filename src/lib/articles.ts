114181e4440fa7dd9c8cdfe9eb1a2ae55ae85077
daichi-s <d.shiwaku.dev@gmail.com>
2021-08-01 02:56:27 +0900

    :sparkles: mdx読み込み、ページ表示まで実装

---
+++ src/lib/articles.ts
@@ -0,0 +1,78 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import remark from 'remark';
+import html from 'remark-html';
+
+const articlesDirectory = path.join(process.cwd(), 'src/articles');
+
+type ArticlesData = {
+  id: string,
+  title: string,
+  date: Date
+}
+
+/**
+ * mdxのメタデータを取得する
+ *
+ * @returns {ArticlesData}
+ */
+export function getSortedArticlesData() {
+  const fileNames = fs.readdirSync(articlesDirectory);
+  const allArticlesData = fileNames.map((fileName) => {
+    const id = fileName.replace(/\.mdx$/, '');
+
+    const fullPath = path.join(articlesDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+    const matterResult = matter(fileContents);
+
+    return {
+      id,
+      ...matterResult.data,
+    } as ArticlesData;
+  });
+
+  return allArticlesData.sort((a, b) => {
+    if (a.date < b.date) {
+      return 1;
+    }
+    return -1;
+  });
+}
+
+/**
+ * mdxのファイル名(ID)を取得する
+ */
+export function getAllArticleIds() {
+  const fileNames = fs.readdirSync(articlesDirectory);
+  return fileNames.map((fileName) => ({
+    params: {
+      id: fileName.replace(/\.mdx$/, ''),
+    },
+  }));
+}
+
+/**
+ * 対象のmdxのメタデータと記事(HTML)を取得する
+ *
+ * @param id
+ * @returns
+ */
+export async function getArticleData(id: string) {
+  const fullPath = path.join(articlesDirectory, `${id}.mdx`);
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+  const matterResult = matter(fileContents);
+
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content);
+  const contentHtml = processedContent.toString();
+
+  return {
+    id,
+    contentHtml,
+    ...matterResult.data,
+  };
+}
---
+++ src/pages/article/[id].tsx
@@ -0,0 +1,40 @@
+import Layout from '../../components/layouts/layout';
+import { getAllArticleIds, getArticleData } from '../../lib/articles';
+
+type ArticleData = {
+    id: string;
+    contentHtml: string;
+    title: string;
+    date: string;
+};
+
+type Props = {
+    articleData: ArticleData;
+};
+
+export default function Post({ articleData }: Props) {
+  return (
+    <Layout>
+      <h2>{articleData.title}</h2>
+      <div dangerouslySetInnerHTML={{ __html: articleData.contentHtml }} />
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = getAllArticleIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }: {params: {id: string}}) {
+    const articleData = await getArticleData(params.id);
+    console.log(articleData);
+  return {
+    props: {
+      articleData,
+    },
+  };
+}
