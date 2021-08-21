d47c083d2ee7dc35d1e09d15b6132e32463ae109
daichi-s <d.shiwaku.dev@gmail.com>
2021-08-22 00:38:15 +0900

    :art: style調整

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -1,17 +1,20 @@
 import Link from 'next/link';
 
 const Footer = () => {
+  // TODO: 共通化
+  const textClass = 'text-gray-500 font-bold';
+
   return (
     <footer className="text-center">
       <div className="pb-5">
         <div className="mb-5">
-          <Link href="/"><a>HOME</a></Link>
+          <Link href="/"><a className={textClass}>HOME</a></Link>
           <span className="mx-3">|</span>
-          <Link href="/policy"><a>プライバシーポリシー</a></Link>
+          <Link href="/policy"><a className={textClass}>プライバシーポリシー</a></Link>
           <span className="mx-3">|</span>
-          <Link href="/term"><a>免責事項</a></Link>
+          <Link href="/term"><a className={textClass}>免責事項</a></Link>
         </div>
-        <div className="mb-5">
+        <div className={`mb-5 ${textClass}`} >
           © 2021 {process.env.BLOG_TITLE}
         </div>
       </div>
--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header className="bg-white shadow">
       <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <div className="text-3xl font-bold text-gray-900">{process.env.BLOG_TITLE}</div>
+        <div className="text-3xl font-bold text-gray-500">{process.env.BLOG_TITLE}</div>
       </div>
     </header>
   );
